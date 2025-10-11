637c030c298083d75fbae5cbbc0e491958bc5a38
Bojin Li <git@bojin.li>
2025-10-11 16:11:06 +1100

    feat: update to hyper 1.x and refactor proxy for new hyper API
    Switch to hyper 1.x, hyper-util, and http-body-util. Refactor proxy
    implementation to use new connection APIs and remove deprecated types.
    Add custom error handling and update request forwarding logic.

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,17 +1,77 @@
 // proxy.rs
+use bytes::Bytes;
 use cidr::{Ipv4Cidr, Ipv6Cidr};
+use http_body_util::{combinators::BoxBody, BodyExt, Empty};
 use hyper::{
-    client::HttpConnector,
-    server::conn::AddrStream,
-    service::{make_service_fn, service_fn},
-    Body, Client, Method, Request, Response, Server,
+    body::Incoming, client::conn::http1 as client_http1, server::conn::http1 as server_http1,
+    service::service_fn, Method, Request, Response,
 };
+use hyper_util::rt::TokioIo;
 use rand::Rng;
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, ToSocketAddrs};
+use std::{
+    error::Error as StdError,
+    fmt,
+    io::{self, ErrorKind},
+    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, ToSocketAddrs},
+};
 use tokio::{
     io::{AsyncRead, AsyncWrite},
-    net::TcpSocket,
+    net::{TcpListener, TcpSocket, TcpStream},
 };
+
+type ProxyResult<T> = Result<T, ProxyError>;
+type ProxyBody = BoxBody<Bytes, hyper::Error>;
+
+fn empty_body() -> ProxyBody {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+#[derive(Debug)]
+enum ProxyError {
+    Hyper(hyper::Error),
+    Io(io::Error),
+    Message(&'static str),
+}
+
+impl fmt::Display for ProxyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ProxyError::Hyper(err) => write!(f, "hyper error: {err}"),
+            ProxyError::Io(err) => write!(f, "io error: {err}"),
+            ProxyError::Message(msg) => write!(f, "{msg}"),
+        }
+    }
+}
+
+impl StdError for ProxyError {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        match self {
+            ProxyError::Hyper(err) => Some(err),
+            ProxyError::Io(err) => Some(err),
+            ProxyError::Message(_) => None,
+        }
+    }
+}
+
+impl From<hyper::Error> for ProxyError {
+    fn from(err: hyper::Error) -> Self {
+        ProxyError::Hyper(err)
+    }
+}
+
+impl From<io::Error> for ProxyError {
+    fn from(err: io::Error) -> Self {
+        ProxyError::Io(err)
+    }
+}
+
+impl From<&'static str> for ProxyError {
+    fn from(msg: &'static str) -> Self {
+        ProxyError::Message(msg)
+    }
+}
 
 #[derive(Clone, Default)]
 pub struct ProxyConfig {
@@ -23,22 +83,33 @@ pub async fn start_proxy(
     listen_addr: SocketAddr,
     config: ProxyConfig,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let make_service = make_service_fn(move |_: &AddrStream| {
-        let config = config.clone();
-        async move {
-            Ok::<_, hyper::Error>(service_fn(move |req| {
-                let config = config.clone();
-                Proxy { config }.proxy(req)
-            }))
-        }
-    });
+    let listener = TcpListener::bind(listen_addr).await?;
 
-    Server::bind(&listen_addr)
-        .http1_preserve_header_case(true)
-        .http1_title_case_headers(true)
-        .serve(make_service)
-        .await
-        .map_err(|err| err.into())
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let config_clone = config.clone();
+
+        tokio::task::spawn(async move {
+            let proxy = Proxy {
+                config: config_clone,
+            };
+            let io = TokioIo::new(stream);
+            let service = service_fn(move |req| {
+                let proxy = proxy.clone();
+                async move { proxy.proxy(req).await }
+            });
+
+            if let Err(err) = server_http1::Builder::new()
+                .preserve_header_case(true)
+                .title_case_headers(true)
+                .serve_connection(io, service)
+                .with_upgrades()
+                .await
+            {
+                eprintln!("connection error: {err}");
+            }
+        });
+    }
 }
 
 #[derive(Clone)]
@@ -47,41 +118,67 @@ pub(crate) struct Proxy {
 }
 
 impl Proxy {
-    pub(crate) async fn proxy(self, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-        match if req.method() == Method::CONNECT {
+    async fn proxy(self, req: Request<Incoming>) -> ProxyResult<Response<ProxyBody>> {
+        if req.method() == Method::CONNECT {
             self.process_connect(req).await
         } else {
             self.process_request(req).await
-        } {
-            Ok(resp) => Ok(resp),
-            Err(e) => Err(e),
         }
     }
 
-    async fn process_connect(self, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    async fn process_connect(self, req: Request<Incoming>) -> ProxyResult<Response<ProxyBody>> {
         tokio::task::spawn(async move {
             let remote_addr = req.uri().authority().map(|auth| auth.to_string()).unwrap();
-            let mut upgraded = hyper::upgrade::on(req).await.unwrap();
-            self.tunnel(&mut upgraded, remote_addr).await
+            let upgraded = hyper::upgrade::on(req).await.unwrap();
+            let mut upgraded = TokioIo::new(upgraded);
+            if let Err(err) = self.tunnel(&mut upgraded, remote_addr).await {
+                eprintln!("tunnel error: {err}");
+            }
         });
-        Ok(Response::new(Body::empty()))
+        Ok(Response::new(empty_body()))
     }
 
-    async fn process_request(self, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-        let bind_addr = self.get_rand_ip();
-        let mut http = HttpConnector::new();
-        http.set_local_address(Some(bind_addr));
-        println!("{} via {bind_addr}", req.uri().host().unwrap_or_default());
+    async fn process_request(self, req: Request<Incoming>) -> ProxyResult<Response<ProxyBody>> {
+        let authority = req
+            .uri()
+            .authority()
+            .ok_or("request missing authority")?
+            .clone();
+        let host = authority.host().to_string();
+        let port = authority
+            .port_u16()
+            .unwrap_or_else(|| match req.uri().scheme_str() {
+                Some("https") => 443,
+                _ => 80,
+            });
 
-        let client = Client::builder()
-            .http1_title_case_headers(true)
-            .http1_preserve_header_case(true)
-            .build(http);
-        let res = client.request(req).await?;
-        Ok(res)
+        let stream = self.connect_to_host(&host, port).await?;
+        if let Ok(addr) = stream.local_addr() {
+            println!("{} via {}", host, addr.ip());
+        } else {
+            println!("{host} via <unknown>");
+        }
+
+        let io = TokioIo::new(stream);
+        let (mut sender, connection) = client_http1::Builder::new()
+            .preserve_header_case(true)
+            .title_case_headers(true)
+            .handshake(io)
+            .await?;
+
+        tokio::task::spawn(async move {
+            if let Err(err) = connection.without_shutdown().await {
+                eprintln!("upstream connection error: {err}");
+            }
+        });
+
+        let res = sender.send_request(req).await?;
+        let (parts, body) = res.into_parts();
+        let body = body.map_err(|err| err).boxed();
+        Ok(Response::from_parts(parts, body))
     }
 
-    async fn tunnel<A>(self, upgraded: &mut A, addr_str: String) -> std::io::Result<()>
+    async fn tunnel<A>(self, upgraded: &mut A, addr_str: String) -> io::Result<()>
     where
         A: AsyncRead + AsyncWrite + Unpin + ?Sized,
     {
@@ -91,7 +188,10 @@ impl Proxy {
                     SocketAddr::V4(_) => TcpSocket::new_v4()?,
                     SocketAddr::V6(_) => TcpSocket::new_v6()?,
                 };
-                let bind_addr = self.get_rand_socket_addr();
+                let bind_addr = match self.get_rand_socket_addr_for(&addr) {
+                    Some(addr) => addr,
+                    None => continue,
+                };
                 if socket.bind(bind_addr).is_ok() {
                     println!("{addr_str} via {bind_addr}");
                     if let Ok(mut server) = socket.connect(addr).await {
@@ -107,21 +207,70 @@ impl Proxy {
         Ok(())
     }
 
-    fn get_rand_ip(&self) -> IpAddr {
-        let mut rng = rand::thread_rng();
-        if rng.gen_bool(0.5) && self.config.ipv6.is_some() {
-            IpAddr::V6(self.get_rand_ipv6())
-        } else if self.config.ipv4.is_some() {
-            IpAddr::V4(self.get_rand_ipv4())
-        } else {
-            IpAddr::V6(self.get_rand_ipv6()) // Fallback to IPv6 if IPv4 is not configured
-        }
+    async fn connect_to_host(&self, host: &str, port: u16) -> io::Result<TcpStream> {
+        let addrs = tokio::net::lookup_host((host, port)).await?;
+        self.connect_to_first(addrs).await
     }
 
-    fn get_rand_socket_addr(&self) -> SocketAddr {
-        let ip = self.get_rand_ip();
+    async fn connect_to_first<I>(&self, addrs: I) -> io::Result<TcpStream>
+    where
+        I: IntoIterator<Item = SocketAddr>,
+    {
+        let mut last_err = None;
+        let mut attempted = false;
+
+        for addr in addrs {
+            let socket = match addr {
+                SocketAddr::V4(_) => TcpSocket::new_v4()?,
+                SocketAddr::V6(_) => TcpSocket::new_v6()?,
+            };
+
+            let bind_addr = match self.get_rand_socket_addr_for(&addr) {
+                Some(addr) => addr,
+                None => continue,
+            };
+
+            attempted = true;
+
+            if let Err(err) = socket.bind(bind_addr) {
+                last_err = Some(err);
+                continue;
+            }
+
+            match socket.connect(addr).await {
+                Ok(stream) => return Ok(stream),
+                Err(err) => {
+                    last_err = Some(err);
+                }
+            }
+        }
+
+        if !attempted {
+            return Err(io::Error::new(
+                ErrorKind::AddrNotAvailable,
+                "no available address family for destination",
+            ));
+        }
+
+        Err(last_err.unwrap_or_else(|| {
+            io::Error::new(ErrorKind::Other, "unable to connect to destination")
+        }))
+    }
+
+    fn get_rand_socket_addr_for(&self, remote: &SocketAddr) -> Option<SocketAddr> {
         let port = rand::thread_rng().gen::<u16>();
-        SocketAddr::new(ip, port)
+        match remote {
+            SocketAddr::V4(_) => self
+                .config
+                .ipv4
+                .as_ref()
+                .map(|_| SocketAddr::new(IpAddr::V4(self.get_rand_ipv4()), port)),
+            SocketAddr::V6(_) => self
+                .config
+                .ipv6
+                .as_ref()
+                .map(|_| SocketAddr::new(IpAddr::V6(self.get_rand_ipv6()), port)),
+        }
     }
 
     fn get_rand_ipv6(&self) -> Ipv6Addr {
